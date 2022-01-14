54472eef88868ff96528e0c5f2450cd86ae3d98f
vincedchart <82042821+vincedchart@users.noreply.github.com>
2022-01-14 11:34:54 +0100

    Create mfy-espana.js

---
+++ mfy-espana.js
@@ -0,0 +1,299 @@
+// Essai de groupement non encore rÃ©ussi 
+
+ 
+
+  //        var monum = L.WMS.overlay('http://mappingforyou.eu/geoserver/wms?', { 
+
+// layers: 'france.patrim.mhareas', 
+
+// 'format': 'image/png', 
+
+// 'transparent': true, 
+
+ //     }); 
+
+ //     var mon = L.layerGroup([featureLayer, monum]); 
+
+ 
+
+ 
+
+// Categories 
+
+ 
+
+// panel layers 
+
+// base layers fonctionne sur la mÃªme architecture, mais je n'en veux pas donc cachÃ© 
+
+var baseLayers = [ 
+
+// { 
+
+// name: "Carte paysagÃ¨re", 
+
+// layer: vdlibraries 
+
+// }, 
+
+]; 
+
+  
+
+
+
+
+var overLayers = [
+	{
+		group: " ",
+		layers: [
+		
+						{
+		name: "Vista aÃ©rea (aerial view)",
+	layer: L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+
+		})
+			},
+		
+			{
+				active: false,
+				name: "Alojamiento (accommodation)",
+				icon: '<i class="icon icon-tente"></i>',
+				layer: {
+					type: "tileLayer.wms",
+					args: ["http://mappingforyou.eu/geoserver/wms", {
+							layers: 'es.poi.hebergement',
+							format: 'image/png',
+							transparent: true,
+							maxZoom: 20
+							//minZoom: 13
+						}
+					]
+				}
+							},
+					{
+				active: false,
+				name: "Restauracion (eating out)",
+				icon: '<i class="icon icon-restaurant"></i>',
+				layer: {
+					type: "tileLayer.wms",
+					args: ["http://mappingforyou.eu/geoserver/wms", {
+							layers: 'es.poi.barrest',
+							format: 'image/png',
+							transparent: true,
+							maxZoom: 20
+							//minZoom: 13
+						}
+					]
+				}
+							},
+					{
+				active: false,
+				name: "Tiendas (stores)",
+				icon: '<i class="icon icon-supermarche"></i>',
+				layer: {
+					type: "tileLayer.wms",
+					args: ["http://mappingforyou.eu/geoserver/wms", {
+							layers: 'es.poi.stores',
+							format: 'image/png',
+							transparent: true,
+							maxZoom: 20
+							//minZoom: 13
+						}
+					]
+				}
+						},
+			
+								{
+				active: false,
+				name: "Servicios/emerg. (services/emerg.)",
+				icon: '<i class="icon icon-emergency"></i>',
+				layer: {
+					type: "tileLayer.wms",
+					args: ["http://mappingforyou.eu/geoserver/wms", {
+							layers: 'es.poi.services',
+							format: 'image/png',
+							transparent: true,
+							maxZoom: 20
+							//minZoom: 13
+						}
+					]
+				}
+							},
+							
+								{
+				active: false,
+				name: "Aficiones (going out)",
+				icon: '<i class="icon icon-loisirs"></i>',
+				layer: {
+					type: "tileLayer.wms",
+					args: ["http://mappingforyou.eu/geoserver/wms", {
+							layers: 'es.poi.loisirs',
+							format: 'image/png',
+							transparent: true,
+							maxZoom: 20
+							//minZoom: 13
+						}
+					]
+				}
+
+			},
+
+
+												{
+				active: false,
+				name: "Fotos Flickr (photos)",
+				icon: '<i class="icon icon-monument"></i>',
+				layer: flickr,
+							},
+						
+						
+	
+	
+
+{ 
+
+name: "Carreteras (roads)", 
+
+icon: '<i class="icon icon-road"></i>', 
+
+layer: L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lines/{z}/{x}/{y}{r}.{ext}', { 
+
+    maxZoom:20, 
+
+  
+
+}) 
+
+}, 
+
+			
+
+{ 
+
+name: "LÃ­neas de bicicleta (cycling)", 
+
+icon: '<i class="icon icon-velo"></i>', 
+
+layer: L.tileLayer('https://tile.waymarkedtrails.org/cycling/{z}/{x}/{y}.png', { 
+
+    maxZoom:20, 
+
+  
+
+}) 
+
+}, 
+
+		
+		
+		
+ 
+
+{ 
+
+name: "Senderismo (hiking)", 
+
+icon: '<i class="icon icon-randonnee"></i>', 
+
+layer: L.tileLayer('https://tile.waymarkedtrails.org/hiking/{z}/{x}/{y}.png', { 
+
+    maxZoom:20, 
+
+  
+
+}) 
+
+}, 
+
+ 
+
+{ 
+
+name: "EsquÃ­ (skiing)", 
+
+icon: '<i class="icon icon-mountains"></i>', 
+
+layer: L.tileLayer('https://tiles.opensnowmap.org/pistes/{z}/{x}/{y}.png', { 
+
+    maxZoom:20, 
+
+  
+
+}) 
+
+}, 
+
+ 
+  
+
+	
+										{
+				active: false,
+				name: "LÃ­nea de contorno (contour lines)",
+				icon: '<i class="icon icon-mountains"></i>',
+				layer: {
+					type: "tileLayer.wms",
+					args: ["https://ows.terrestris.de/osm/service?", {
+							layers: 'SRTM30-Contour',
+							format: 'image/png',
+							transparent: true,
+							//minZoom: 13
+						}
+					]
+				}
+				
+			},
+	
+	
+	
+{ 
+
+active: false, 
+
+name: "Elementos dibujados (drawn items)", 
+
+icon: '<i class="icon icon-monument"></i>', 
+
+layer: drawnItems, 
+
+}, 
+	
+	
+	
+
+			
+	//		{
+	//			active: true,
+	//			name: "River",
+	//			layer: (function() {
+	//				var l = L.geoJson();
+	//				$.getJSON('data/river.json', function(j) {
+	//					l.addData(j);
+	//				});
+	//				return l;
+	//			}())
+	//		},
+		]
+	},
+	
+];
+
+
+
+  
+
+// panel
+
+var panelLayers = new L.Control.PanelLayers(baseLayers, overLayers, { 
+
+compact: true, 
+
+collapsed: true, 
+
+collapsibleGroups: true 
+
+}); 
+
+  
+map.addControl(panelLayers);

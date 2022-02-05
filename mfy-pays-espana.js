2a351e8650a9fbfe74407f08daf6aba8d0db39b3
vincedchart <82042821+vincedchart@users.noreply.github.com>
2022-02-05 13:46:37 +0100

    Update and rename mfy-pays-espana to mfy-pays-espana.js

--- mfy-pays-espana.js
+++ mfy-pays-espana.js
@@ -4,7 +4,7 @@ var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 var hillshading = L.tileLayer('http://{s}.tiles.wmflabs.org/hillshading/{z}/{x}/{y}.png', { maxZoom: 20, opacity: 0  }); 
   
 var mapbase = L.tileLayer.wms('https://mappingforyou.eu/geoserver/wms?', { 
-       layers: 'pays.deutschland', 
+       layers: 'pays.espana', 
        maxZoom: 20, 
        format: 'image/png', 
        attribution: '↖Menu . Categories↗)' 
@@ -29,7 +29,7 @@ flickrAPI += "&api_key=11d9a83951636a94885f5145f1a33727";
 //flickrAPI += "&user_id=194715090%40N08"; 
 flickrAPI += "&has_geo=1"; 
 // essai pour local 
-flickrAPI += "&tags=Germany" 
+flickrAPI += "&tags=Spain" 
 flickrAPI += "&extras=geo,url_t,owner_name,date_upload,license"; 
 //flickrAPI += "&per_page=5";  // Nº máximo de fotos para visualizar 
 //flickrAPI += "&page=1"; 

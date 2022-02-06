3846e85feea5513f668f45f3b2f28c3ae9e536ad
vincedchart <82042821+vincedchart@users.noreply.github.com>
2022-02-06 16:17:56 +0100

    Update mfy-addons.js

--- mfy-addons.js
+++ mfy-addons.js
@@ -1,35 +1,5 @@
 
 
-// print
-var customActionToPrint = function(context, mode) {
-	return function() {
-		window.alert("We are printing the MAP. Let's do Custom print here!");
-		context._printMode(mode);
-	}
-};
-
-L.control.browserPrint({
-	title: 'Just print me!',
-	documentTitle: 'Map printed using leaflet.browser.print plugin',
-	printLayer: L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
-					attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-					subdomains: 'abcd',
-					minZoom: 1,
-					maxZoom: 16,
-					ext: 'png'
-				}),
-	closePopupsOnPrint: false,
-	printModes: [
-            L.BrowserPrint.Mode.Landscape("Tabloid",{title: "Tabloid VIEW"}),
-            L.browserPrint.mode("Alert",{title:"User specified print action",pageSize: "A6", action: customActionToPrint, invalidateBounds: false}),
-            L.BrowserPrint.Mode.Landscape(),
-            "Portrait",
-            L.BrowserPrint.Mode.Auto("B4",{title: "Auto"}),
-            L.BrowserPrint.Mode.Custom("B5",{title:"Select area"})
-	],
-	manualMode: false
-}).addTo(map);
-
 // scale
 
 L.Control.Scale().remove();	
@@ -58,9 +28,6 @@ minWidth: 300
 
 }); 
 
-  
-
- 
 
 
 // Get JSON request 
@@ -261,3 +228,37 @@ function onMoveEnd(evt) {
   }
 }
 map.on('moveend', onMoveEnd);
+
+
+
+
+
+// print
+var customActionToPrint = function(context, mode) {
+	return function() {
+		window.alert("We are printing the MAP. Let's do Custom print here!");
+		context._printMode(mode);
+	}
+};
+
+L.control.browserPrint({
+	title: 'Just print me!',
+	documentTitle: 'Map printed using leaflet.browser.print plugin',
+	printLayer: L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
+					attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+					subdomains: 'abcd',
+					minZoom: 1,
+					maxZoom: 16,
+					ext: 'png'
+				}),
+	closePopupsOnPrint: false,
+	printModes: [
+            L.BrowserPrint.Mode.Landscape("Tabloid",{title: "Tabloid VIEW"}),
+            L.browserPrint.mode("Alert",{title:"User specified print action",pageSize: "A6", action: customActionToPrint, invalidateBounds: false}),
+            L.BrowserPrint.Mode.Landscape(),
+            "Portrait",
+            L.BrowserPrint.Mode.Auto("B4",{title: "Auto"}),
+            L.BrowserPrint.Mode.Custom("B5",{title:"Select area"})
+	],
+	manualMode: false
+}).addTo(map);

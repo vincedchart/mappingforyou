af09d2ac351b22650bfe4c0b99573c58a486e0ea
vincedchart <82042821+vincedchart@users.noreply.github.com>
2022-02-06 15:49:07 +0100

    Update mfy-addons.js

--- mfy-addons.js
+++ mfy-addons.js
@@ -1,3 +1,36 @@
+// print
+
+
+
+var customActionToPrint = function(context, mode) {
+	return function() {
+		window.alert("We are printing the MAP. Let's do Custom print here!");
+		context._printMode(mode);
+	}
+};
+
+L.control.browserPrint({
+	title: 'Just print me!',
+	documentTitle: 'Map printed using leaflet.browser.print plugin',
+	printLayer: L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
+					attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+					subdomains: 'abcd',
+					minZoom: 1,
+					maxZoom: 16,
+					ext: 'png'
+				}),
+	closePopupsOnPrint: false,
+	printModes: [
+            L.BrowserPrint.Mode.Landscape("Tabloid",{title: "Tabloid VIEW"}),
+            L.browserPrint.mode("Alert",{title:"User specified print action",pageSize: "A6", action: customActionToPrint, invalidateBounds: false}),
+            L.BrowserPrint.Mode.Landscape(),
+            "Portrait",
+            L.BrowserPrint.Mode.Auto("B4",{title: "Auto"}),
+            L.BrowserPrint.Mode.Custom("B5",{title:"Select area"})
+	],
+	manualMode: false
+}).addTo(map);
+
 // photos de son flickr 
 
  
